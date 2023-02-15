4ac0fdd38e5f2fc7e624ee76002348485abfffe1
Kateryna Dubinina <ururruu@gmail.com>
2023-02-15 12:14:54 +0200

    check if it's true

--- server/config.py
+++ server/config.py
@@ -6,4 +6,4 @@ from server.utils import singleton
 @singleton
 class Config:
     def __init__(self):
-        self.local_depployment = os.environ.get('LOCAL_DEPLOYMENT')
+        self.local_depployment = os.environ.get('LOCAL_DEPLOYMENT').lower() == 'true'
