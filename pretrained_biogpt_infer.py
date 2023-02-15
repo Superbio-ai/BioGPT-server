450a2eaaa9d93785f986731296e9cd374e5d59ae
Kateryna Dubinina <ururruu@gmail.com>
2023-02-15 11:42:41 +0200

    all changes to make it work

--- pretrained_biogpt_infer.py
+++ pretrained_biogpt_infer.py
@@ -13,7 +13,7 @@ m = TransformerLanguageModel.from_pretrained(
 
 #m.cuda()
 
-src_tokens = m.encode("COVID-19 is")
+src_tokens = m.encode("Covid-19 is")
 generate = m.generate([src_tokens], beam=5)[0]
 output = m.decode(generate[0]["tokens"])
 
